f2514cb3475473d336255bbd5f6564d776ec4bb0
egatetama <egatetama34@gmail.com>
2024-12-11 22:55:11 +0700

    Update

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <gtk/gtk.h>
+
+static void on_activate(GtkApplication* app, gpointer user_data) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello GTK");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app = gtk_application_new("com.example.HelloGTK", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return status;
+}
