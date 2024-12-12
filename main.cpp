d7320c3952cc58a2a201d935f48118c5a2b9a2f7
egatetama <egatetama34@gmail.com>
2024-12-13 01:54:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
-#include <gtk/gtk.h>
+#include <iostream>
+#include "game1.h"  // Meng-include file header game1.h
 
-static void on_activate(GtkApplication* app, gpointer user_data) {
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Hello GTK");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-    gtk_widget_show_all(window);
-}
+using namespace std;
 
-int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("com.example.HelloGTK", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-    int status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
-    return status;
+int main() {
+    int choice;
+    char playAgain;
+
+    do {
+        // Menu pilihan game
+        cout << "Pilih game yang ingin dimainkan:" << endl;
+        cout << "1. Tic-Tac-Toe" << endl;
+        cout << "2. Game 2" << endl;
+        cout << "Masukkan pilihan Anda (1 atau 2): ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                playTicTacToe();  // Panggil fungsi playTicTacToe dari game1.cpp
+                break;
+            case 2:
+                // Panggil game 2 jika ada
+                break;
+            default:
+                cout << "Pilihan tidak valid." << endl;
+        }
+
+        // Tanyakan apakah pengguna ingin bermain lagi
+        cout << "Apakah Anda ingin bermain lagi? (Y/N): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'Y' || playAgain == 'y');  // Ulangi jika memilih Y
+
+    cout << "Terima kasih telah bermain!" << endl;
+    return 0;
 }
